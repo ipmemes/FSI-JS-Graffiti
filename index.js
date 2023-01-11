516a75031e894a289897759c8455827f3498bca8
Rotseni <accesslife.inc@gmail.com>
2023-01-10 22:48:14 -0800

    domPractice

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Select some elements...
+
+
+
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+}
